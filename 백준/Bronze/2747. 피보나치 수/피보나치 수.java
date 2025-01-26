e77ad6eb378a6239c333f9e2477ecb1c0c2a62b5
Kim Jun Hyeong <kjunh972@gmail.com>
2025-01-26 18:59:16 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n>45) {
+			n = sc.nextInt();
+		}
+		
+		int[] sum = new int[n+1];
+		
+		for (int i=0; i<n+1; i++)
+		{
+			if (i==0) {
+				sum[i+1] =1;
+			} else {
+				if (i<n)
+					sum[i+1] = sum[i]+sum[i-1];
+			}
+		}
+		
+		System.out.println(sum[n]);
+	}
+}
